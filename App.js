cd9c07fdea811a1341af2cb7823db6fbbcb53b70
Edward Balmer <edwardbalmer@gmail.com>
2020-08-12 20:08:27 +0100

    initialising react-navigation

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Screen 1"
+        onPress={() => navigation.navigate('1')}
+      />
+      <Button
+        title="Go to Screen 2"
+        onPress={() => navigation.navigate('2')}
+      />
+      <Button
+        title="Go to Screen 3"
+        onPress={() => navigation.navigate('3')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Screen1() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Screen 1</Text>
+    </View>
+  );
+}
+
+function Screen2() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Screen 2</Text>
+    </View>
+  );
+}
+
+function Screen3() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Screen 3</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="1" component={Screen1} />
+        <Stack.Screen name="2" component={Screen2} />
+        <Stack.Screen name="3" component={Screen3} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
